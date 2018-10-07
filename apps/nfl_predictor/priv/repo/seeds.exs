115120999478e43a30779bc44cf4bc2b137181d9
Anthony Falzetti <anthony.falzetti@gmail.com>
2018-10-07 05:56:05 -0600

    Seed Stadiums

--- apps/nfl_predictor/priv/repo/seeds.exs
+++ apps/nfl_predictor/priv/repo/seeds.exs
@@ -1,11 +1,36 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     NflPredictor.Repo.insert!(%NflPredictor.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+defmodule NflPredictor.Seeds.Stadiums do
+  alias NflPredictor.Nfl.Stadium
+  alias NflPredictor.Repo
+
+  def seed do
+    headers = [ :name, :capacity, :city, :state, :playing_surface, :roof_type, :elevation, :latitude, :longitude ]
+    File.stream!("./priv/repo/seeds/csvs/stadiums.csv")
+    |> CSV.decode!(headers: headers, strip_fields: true)
+    |> Enum.each(&(store_stadium&1))
+  end
+
+  def store_stadium(row) do
+    formatted_row = format_row(row)
+    Stadium.changeset(%Stadium{}, formatted_row)
+    |> Repo.insert!(cs)
+  end
+
+  defp format_row(row) do
+    %{
+      capacity: to_i(row.capacity),
+      city: row.city,
+      elevation: to_i(row.elevation),
+      latitude: to_f(row.latitude),
+      longitude: to_f(row.longitude),
+      name: row.name,
+      playing_surface: row.playing_surface,
+      roof_type: row.roof_type,
+      state: row.state
+    }
+  end
+
+  defp to_i(str), do: String.to_integer(str)
+  defp to_f(str), do: String.to_float(str)
+end
+
+NflPredictor.Seeds.Stadiums.seed
