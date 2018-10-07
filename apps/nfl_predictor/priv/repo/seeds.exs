e8c4937e96d2e3d0033c78a582289a6175b87889
Anthony Falzetti <anthony.falzetti@gmail.com>
2018-10-07 06:13:20 -0600

    Seed conferences and divisions

--- apps/nfl_predictor/priv/repo/seeds.exs
+++ apps/nfl_predictor/priv/repo/seeds.exs
@@ -1,18 +1,36 @@
-defmodule NflPredictor.Seeds.Stadiums do
-  alias NflPredictor.Nfl.Stadium
+defmodule NflPredictor.Seeds do
+  alias NflPredictor.Nfl.{Stadium, Conference, Division}
   alias NflPredictor.Repo
 
   def seed do
+    seed_stadiums
+    seed_confrences
+    seed_divisions
+  end
+
+  defp seed_stadiums do
     headers = [ :name, :capacity, :city, :state, :playing_surface, :roof_type, :elevation, :latitude, :longitude ]
     File.stream!("./priv/repo/seeds/csvs/stadiums.csv")
     |> CSV.decode!(headers: headers, strip_fields: true)
     |> Enum.each(&(store_stadium&1))
   end
 
+  defp seed_confrences do
+    %Conference{name: "afc"} |> Repo.insert!
+    %Conference{name: "nfc"} |> Repo.insert!
+  end
+
+  defp seed_divisions do
+    %Division{name: "west"} |> Repo.insert!
+    %Division{name: "east"} |> Repo.insert!
+    %Division{name: "north"} |> Repo.insert!
+    %Division{name: "south"} |> Repo.insert!
+  end
+
   def store_stadium(row) do
     formatted_row = format_row(row)
     Stadium.changeset(%Stadium{}, formatted_row)
-    |> Repo.insert!(cs)
+    |> Repo.insert!
   end
 
   defp format_row(row) do
@@ -33,4 +51,4 @@ defmodule NflPredictor.Seeds.Stadiums do
   defp to_f(str), do: String.to_float(str)
 end
 
-NflPredictor.Seeds.Stadiums.seed
+NflPredictor.Seeds.seed
